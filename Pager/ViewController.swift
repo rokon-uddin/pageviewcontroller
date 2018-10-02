1f65c2d71f07c0a0c4ce1bf02720677547c40f64
Mohammed Rokon Uddin <rokon.uddin@sekai-lab-bd.com>
2018-10-02 15:10:17 +0600

    Initial Commit

---
+++ Pager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pager
+//
+//  Created by Mohammed Rokon Uddin on 2/10/18.
+//  Copyright © 2018 Monstar Lab Bangladesh Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
